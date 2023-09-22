2b575502b1a6f3fe2e27b46d61142d1ec18c06fc
Ming-der Wang <mingderwang@gmail.com>
2023-09-22 23:20:19 +0800

    tab example ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { createMemo } from "solid-js"
-import * as toggle from "@zag-js/toggle"
-import { useMachine, normalizeProps } from "@zag-js/solid"
+import * as tabs from "@zag-js/tabs"
+import { normalizeProps, useMachine } from "@zag-js/solid"
+import { createMemo, createUniqueId, For } from "solid-js"
+
+const data = [
+  { value: "item-1", label: "Item one", content: "Item one content" },
+  { value: "item-2", label: "Item two", content: "Item two content" },
+  { value: "item-3", label: "Item three", content: "Item three content" },
+]
 
 export default function App() {
-  const [state, send] = useMachine(toggle.machine({
-    id: "1"
-  }))
-  const api = createMemo(() => toggle.connect(state, send, normalizeProps))
+  const [state, send] = useMachine(tabs.machine({ id: createUniqueId(), value: "item-1" }))
+
+  const api = createMemo(() => tabs.connect(state, send, normalizeProps))
+
   return (
-    <button {...api().buttonProps}>
-      {api().isPressed ? "On" : "Off"}
-    </button>
+    <div {...api().rootProps}>
+      <div {...api().tablistProps}>
+        <For each={data}>
+          {(item) => (
+            <button {...api().getTriggerProps({ value: item.value })}>
+              {item.label}
+            </button>
+          )}
+        </For>
+      </div>
+      <For each={data}>
+        {(item) => (
+          <div {...api().getContentProps({ value: item.value })}>
+            <p>{item.content}</p>
+          </div>
+        )}
+      </For>
+    </div>
   )
 }
